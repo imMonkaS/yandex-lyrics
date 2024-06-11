53570f2f5bfe89174dda5f96d9d322865f44a143
MonkaS <danchik646@gmail.com>
2024-06-11 03:19:50 +0300

    CRITICAL переход на externalAPI, работает исправно
    есть ещё что поменять, но работает уже неплохо
    нужно сделать нормальное обращение к апи для скаченных песен
    В идеале конечно найти способ лутать тексты для всех песен, то апи, которое используется сейчас, очень слабое в плане текстов для музыки

--- yandex_music.js
+++ yandex_music.js
@@ -303,70 +303,198 @@
 //     start();
 // }, 3000);
 
-let LAST_SONG = null;
+// let LAST_SONG = null;
 
-new MutationObserver(function(mutations) {
-    setTimeout(()=>{
-        let song_title = getCurrentSong();
+// new MutationObserver(function(mutations) {
+//     setTimeout(()=>{
+//         let song_title = getCurrentSong();
         
-        if (LAST_SONG === null){
-            LAST_SONG = song_title;
-            loadSong(song_title);
-            // console.log("Поменялась");
+//         if (LAST_SONG === null){
+//             LAST_SONG = song_title;
+//             loadSong(song_title);
+//             // console.log("Поменялась");
+//         }
+        
+//         // Делаем если песня сменилась
+//         if (LAST_SONG !== song_title){
+//             LAST_SONG = song_title;
+//             loadSong(song_title);
+//             // console.log("Поменялась");
+//         }
+//         // Делаем если песня не сменилась
+//         else{
+//             // console.log('Не поменялась');
+//         }
+
+//     }, 750);
+// }).observe(
+//     document.querySelector('title'),
+//     { subtree: true, characterData: true, childList: true }
+// );
+
+
+// function getCurrentSong(){
+//     let song = null;
+//     let artist = null;
+//     try{
+//         song = document.querySelector('div.track__name').children[0].children[0].children[0].innerHTML;
+//         artist = document.querySelector('div.track__artists').children[0].children[0].innerHTML;
+//     }
+//     catch(error){
+//         // console.error("error: ", error);
+//     }
+
+//     let song_title = song;
+//     if (song_title.indexOf(' - ') !== -1){
+//         song_title = song_title.split(' - ')[0]
+//     }
+//     if (song_title.indexOf(' (Nightcore)') !== -1){
+//         song_title = song_title.replace(' (Nightcore)', '');
+//     }
+//     if (artist !== null) song_title += ' - ' + artist;
+//     song_title = song_title.replace('.mp3', '');
+
+//     // console.log(song_title);
+
+//     return song_title
+// }
+
+function trackDownloaded(textField, trackTitle){
+    async function fetchAsync (url) {
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        results = [];
+
+        let originalAuthors = []
+        if (data.result.tracks !== undefined){
+            for (res of data.result.tracks.results){
+                if (res.lyricsInfo.hasAvailableTextLyrics && res.lyricsAvailable){
+                    results.push(res.id);
+                    originalAuthors.push([res.title, res.artists[0].name]);
+                }
+            }
+        }
+        texts = []
+        for (r of results){
+            let response1 = await fetch("https://api.music.yandex.net/tracks/" + r + "/supplement");
+            let data1 = await response1.json();
+            if (data1['result']['lyrics'] !== undefined){
+                texts.push(data1['result']['lyrics']['fullLyrics'])
+            }
+        }
+
+        if (document.querySelector('.lyrics-holder-container') !== null){
+            document.querySelector('.lyrics-holder-container').remove()
+        }
+
+        let divContainer = document.createElement('div')
+        divContainer.classList.add("lyrics-holder-container");
+        document.querySelector('.teaser').appendChild(divContainer);
+        
+        // Текст возможно есть в YandexAPI
+        if (texts.length != 0){
+            let leftbtn = document.createElement('button');
+            leftbtn.style.padding = '15px';
+            leftbtn.innerHTML = '<';
+            leftbtn.style.border = 'None';
+            leftbtn.style.outline = 'None';
+            leftbtn.style.background = 'None';
+            leftbtn.style.fontSize = '30px';
+            leftbtn.style.color = '#fff';
+            divContainer.appendChild(leftbtn);
+
+            let pageSpan = document.createElement('span');
+            let Lcounter = 0
+            pageSpan.innerHTML = Lcounter + 1;
+            pageSpan.style.padding = '30px';
+            pageSpan.style.fontSize = '25px';
+            divContainer.appendChild(pageSpan);
+
+            let rightbtn = document.createElement('button');
+            rightbtn.style.padding = '15px';
+            rightbtn.innerHTML = '>';
+            rightbtn.style.border = 'None';
+            rightbtn.style.outline = 'None';
+            rightbtn.style.background = 'None';
+            rightbtn.style.fontSize = '30px';
+            rightbtn.style.color = '#fff';
+            divContainer.appendChild(rightbtn);
+            lyricsChanger();
+            function lyricsChanger(){
+                if (Lcounter > texts.length - 1){
+                    Lcounter = 0;
+                }
+                if (Lcounter < 0){
+                    Lcounter = texts.length - 1
+                }
+                pageSpan.innerHTML = Lcounter + 1;
+
+                textField.innerHTML = "<span style='font-size: 22px; font-weight: bold'>" + trackTitle.split(' - ')[0] + " (" + originalAuthors[Lcounter][1] + " - " + originalAuthors[Lcounter][0] + ")" + "</span>" + "<br> </br>" + texts[Lcounter].replaceAll('\n', '<br> </br>');
+            }
+
+            rightbtn.onclick = function(){
+                Lcounter += 1;
+                lyricsChanger();
+            }
+
+            leftbtn.onclick = function(){
+                Lcounter -= 1;
+                lyricsChanger();
+            }
         }
         
-        // Делаем если песня сменилась
-        if (LAST_SONG !== song_title){
-            LAST_SONG = song_title;
-            loadSong(song_title);
-            // console.log("Поменялась");
-        }
-        // Делаем если песня не сменилась
+        // Текста нет в YandexAPI
         else{
-            // console.log('Не поменялась');
+            textField.innerHTML = "<span style='font-size: 18px'> Для трека <b>" + trackTitle + "</b> нет текста в API.</span>" + "<br> </br>";
         }
 
-    }, 750);
-}).observe(
-    document.querySelector('title'),
-    { subtree: true, characterData: true, childList: true }
-);
-
-
-function getCurrentSong(){
-    let song = null;
-    let artist = null;
-    try{
-        song = document.querySelector('div.track__name').children[0].children[0].children[0].innerHTML;
-        artist = document.querySelector('div.track__artists').children[0].children[0].innerHTML;
+        if (texts.length == 1){
+            divContainer.remove();
+        }
     }
-    catch(error){
-        // console.error("error: ", error);
-    }
-
-    let song_title = song;
-    if (song_title.indexOf(' - ') !== -1){
-        song_title = song_title.split(' - ')[0]
-    }
-    if (song_title.indexOf(' (Nightcore)') !== -1){
-        song_title = song_title.replace(' (Nightcore)', '');
-    }
-    if (artist !== null) song_title += ' - ' + artist;
-    song_title = song_title.replace('.mp3', '');
-
-    // console.log(song_title);
-
-    return song_title
+    fetchAsync("https://api.music.yandex.net/search?text=" + trackTitle.split(' - ')[0] + "&page=0&type=all&nococrrect=false");
 }
 
+function prepareTextField(){
+    let textField = document.querySelector('.sidebar__ads');
+    textField.style.fontSize = '15px';
+    textField.style.padding = '10px 30px';
+    textField.style.paddingBottom = '50px';
+    textField.style.fontFamily = 'YSTextRegular,Arial,Helvetica,sans-serif';
+}
 
+// function show_downloaded_lyrics(trackTitle){
+//     fetch("https://api.music.yandex.net/search?text=" + trackTitle + "&page=0&type=all")
+//     .then(response => response.json())
+//     .then((data) => {
+//         try{
+//             const tracks = data['result']['tracks']['results'].slice(0, 10);
+//             console.log(tracks);
+//         }
+//         catch(error){
+//             // console.error("error: ", error);
+//         }
+//     })
+//     .catch(error => console.error("error: ", error));
+// }
 
-function loadSong(song_title){
-    fetch("https://api.music.yandex.net/search?text=" + song_title + "&page=0&type=all")
+function show_lyrics(trackId, trackTitle){
+    fetch("https://api.music.yandex.net/tracks/" + trackId + "/supplement")
     .then(response => response.json())
     .then((data) => {
         try{
-            console.log(data['result']['tracks']['results'].slice(0, 10));
+            // const data = data['result']['tracks']['results'].slice(0, 10);
+            console.log(data);
+            // Трек есть в YandexAPI
+            let textField = document.querySelector('.sidebar__ads');
+            if (data['result']['lyrics'] !== undefined){
+                let lyrics = data['result']['lyrics']['fullLyrics'];
+                textField.innerHTML = "<span style='font-size: 22px; font-weight: bold'>" + trackTitle + "</span>" + "<br> </br>" + lyrics.replaceAll('\n', '<br> </br>');
+            }
+            else{
+                textField.innerHTML = "<span style='font-size: 18px'> Для трека <b>" + trackTitle + "</b> нет текста в API.</span>" + "<br> </br>";
+            }
         }
         catch(error){
             // console.error("error: ", error);
@@ -375,18 +503,50 @@ function loadSong(song_title){
     .catch(error => console.error("error: ", error));
 }
 
-function getLyrics(url){
-    return fetch(url)
-    .then(response => response.json())
-    .catch(error => console.error("error: ", error));
+// Wait for the Yandex.Music external API to be ready
+window.addEventListener("load", function() {
+    if (typeof externalAPI !== "undefined") {
+        prepareTextField();
+        // Listen for changes in the track
+        externalAPI.on(externalAPI.EVENT_TRACK, function() {
+            const track = externalAPI.getCurrentTrack();
+
+            if (track) {
+                console.log(track);
+                // скачанный
+                if (track.cover === undefined){
+                    // console.log("я скачанный");
+                    const trackTitle = generateTrackTitle(track);
+                    console.log(trackTitle);
+                    // show_downloaded_lyrics(trackTitle);
+                    trackDownloaded(document.querySelector('.sidebar__ads'), trackTitle);
+                }
+                // не скачанный
+                else{
+                    // console.log("я не скачанный");
+                    const trackId = track.link.split('/')[4]
+                    const trackTitle = generateTrackTitle(track);
+                    // console.log(trackId);
+                    show_lyrics(trackId, trackTitle);
+                }
+            }
+        });
+    }
+});
+
+
+function generateTrackTitle(track){
+    let artists = [];
+    for (let artist of track.artists){
+        if (artist.title !== 'Zen - Kun')
+            artists.push(artist.title);
+    }
+    let title = track.title.replace('.mp3', '').replace(' (Nightcore)', '').replace(' - Nightcore', '');
+    if (artists.length !== 0){
+        title += ' - ' + artists.join(', ')
+    }
+    return title;
 }
-
-
-
-
-
-
-
 
 
 
